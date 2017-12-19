eb2c5e8ee06a96c7f3f88bfdfc902aa6caf7fe82
Siddharthaa <34640824+siddharthaaradhakrishnan@users.noreply.github.com>
2017-12-19 12:08:11 +0530

    Update leap year.c

--- Code Kata/leap year.c
+++ Code Kata/leap year.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
-
 int main(void)
 {
-	int a;
-	scanf("%d",&a);
-	if((a%4)==0)
-	printf("Leap Year");
-	else printf("Not a Leap year");
-	
-	
-	
-	return 0;
+ int a;
+ scanf("%d",&a);
+ if(a%4==0)
+ {
+ 	if(a%100==0)
+ 	{
+ 		if(a%400==0)
+ 			printf("It is a Leap Year");
+ 			else 
+ 			printf("Not a Leap Year");
+ 	}
+ 	else printf("It is a Leap Year");
+ }
+ else printf("Not a Leap Year");
+ return 0;
+ 
 }
