f52cae0094a798442ba0565757124d402f0d6328
Siddharthaa <34640824+siddharthaaradhakrishnan@users.noreply.github.com>
2018-03-08 10:27:39 +0530

    Create armstrongnumber.c

---
+++ Code Kata/armstrongnumber.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(void)
+{
+    int n,sum=0,i,a;
+    scanf("%d",&n);
+    a=n;
+    while(a!=0)
+    {
+    	i=a%10;
+    	sum=sum+(i*i*i);
+    	a=a/10;
+    }
+    if(sum==n)
+    {
+    	printf("Armstrong Number");
+    }
+    else
+    {
+    printf("Not a Armstrong Numner");	
+    }
+    return(0);
+}
