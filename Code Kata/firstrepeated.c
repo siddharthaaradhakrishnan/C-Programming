c6df1b9a7d287ec3886e03a369d3cb089ad6b025
Siddharthaa <34640824+siddharthaaradhakrishnan@users.noreply.github.com>
2018-05-17 22:35:50 +0530

    Create firstrepeated.c

---
+++ Code Kata/firstrepeated.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(int i=0;i<n;i++)
+	{
+		int k=0;
+		for(int j=0;j<n;j++)
+		{
+			if(arr[i]==arr[j])
+			{k++;
+			}
+		}
+			if(k==2)
+			{
+			printf("%d\t",arr[i]);
+		exit(0);
+		}
+	}
+printf("unique");
+return 0;
+}
+
